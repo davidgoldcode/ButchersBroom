4c28ad4d301851b7a0e7171f6aa410c00ffe8f44
David Gold <davidgold@hey.com>
2020-10-21 18:00:45 -0400

    fixed server

--- back-end/auth/auth-router.js
+++ back-end/auth/auth-router.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const db = require("./user-model.js");
+const db = require("../users/user-model");
 const router = express.Router();
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
